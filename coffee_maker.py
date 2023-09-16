0fbf65ac9e58b2530dd6bea59113b6924930d436
KlaudjaKaraj <38klaudja.karaj@gmail.com>
2023-09-16 13:01:40 +0200

    adding some cute emojis

--- coffee_maker.py
+++ coffee_maker.py
@@ -2,28 +2,29 @@ class CoffeeMaker:
     """Models the machine that makes the coffee"""
     def __init__(self):
         self.resources = {
-            "water": 300,
-            "milk": 200,
-            "coffee": 100,
+            "water": 5000,
+            "milk": 2000,
+            "coffee": 2000,
         }
 
     def report(self):
-        """Prints a report of all resources."""
-        print(f"Water: {self.resources['water']}ml")
+        #Prints a report of all resources.
+        print("\nHere is your report: 🧾")
+        print(f"Water: {self.resources['water']}ml ")
         print(f"Milk: {self.resources['milk']}ml")
         print(f"Coffee: {self.resources['coffee']}g")
 
     def is_resource_sufficient(self, drink):
-        """Returns True when order can be made, False if ingredients are insufficient."""
+        #Returns True when order can be made, False if ingredients are insufficient.
         can_make = True
         for item in drink.ingredients:
             if drink.ingredients[item] > self.resources[item]:
-                print(f"Sorry there is not enough {item}.")
+                print(f"Sorry ❗There is not enough {item}.")
                 can_make = False
         return can_make
 
     def make_coffee(self, order):
-        """Deducts the required ingredients from the resources."""
+        #Deducts the required ingredients from the resources.
         for item in order.ingredients:
             self.resources[item] -= order.ingredients[item]
-        print(f"Here is your {order.name} ☕️. Enjoy!")
+        print(f"Here is your {order.name} ☕. Enjoy!")
